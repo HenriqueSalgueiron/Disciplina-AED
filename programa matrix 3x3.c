00059a5095235ffef8d61e59e3cb5cc16ae6c4eb
Henrique Salgueiro <92067845+HenriqueSalgueiron@users.noreply.github.com>
2021-12-07 19:50:31 -0300

    Add files via upload

---
+++ programa matrix 3x3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int x, y, cc;
+int matriz[2][2];
+cc=0;
+for (x=0;x<3;x++)
+{
+for (y=0;y<3;y++)
+{
+            
+cc++;
+cc=matriz[x][y];
+printf("%d ", matriz[x][y]);
+}
+printf("\n");
+}
+return 0;
+} 
