72e713ea4952d09c45da9a82aa0ffd0a3639099d
Henrique Salgueiro <92067845+HenriqueSalgueiron@users.noreply.github.com>
2021-12-09 00:35:53 -0300

    Update programa matrix 3x3.c

--- programa matrix 3x3.c
+++ programa matrix 3x3.c
@@ -1,19 +1,16 @@
 #include<stdio.h>
-int main()
-{
-int x, y, cc;
-int matriz[2][2];
-cc=0;
-for (x=0;x<3;x++)
-{
-for (y=0;y<3;y++)
-{
-            
-cc++;
-cc=matriz[x][y];
-printf("%d ", matriz[x][y]);
-}
-printf("\n");
-}
-return 0;
+int main(){
+    
+    int x, y, cc;
+    int matriz[2][2];
+    cc=0;
+    for (x=0;x<3;x++){
+        for (y=0;y<3;y++){
+            cc++;
+            cc=matriz[x][y];
+            printf("%d ", matriz[x][y]);
+        }
+        printf("\n");
+    }
+    return 0;
 } 
