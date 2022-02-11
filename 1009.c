251ad2729c6393baccf57ed85569615da67893b2
Henrique Salgueiro <92067845+HenriqueSalgueiron@users.noreply.github.com>
2022-02-11 17:14:31 -0300

    Update 1009.c

--- 1009.c
+++ 1009.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
-#include<string.h>
+
 int main()
 {
-char x[30];
-double S, M, TOTAL;
-scanf(" %s %lf %lf", &x, &S, &M);
-TOTAL = S + 0.15 * M;
-printf("TOTAL = R$ %.2lf\n", TOTAL);
-return 0;
+    char nome;
+    double salario, vendas, total;
+    
+    scanf("%s", &nome);
+    scanf("%lf", &salario);
+    scanf("%lf", &vendas);
+    
+    total = (salario + (vendas*0.15));
+    
+    printf("TOTAL = R$ %.2lf\n", total);
+    
+    return 0;
+    
+    
+    
+    
+    
+    
+    
 }
