35c034cd57645f1482e333d719fcad59843456cf
Henrique Salgueiro <92067845+HenriqueSalgueiron@users.noreply.github.com>
2022-02-11 16:50:56 -0300

    Create 1008.c

---
+++ 1008.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+int NUMBER, h;
+float n, SALARY;
+scanf("%d %d %f", &NUMBER, &h, &n);
+printf("NUMBER = %d\n", NUMBER);
+SALARY = h * n;
+printf("SALARY = U$ %.2f\n", SALARY);
+return 0;
+}
